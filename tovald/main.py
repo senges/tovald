ee1bc08bbdd9b1acd472ea2a9846719f4d53d15f
Charles SENGES <charles.senges.io+github@protonmail.com>
2024-10-25 16:12:28 +0200

    feat(main.py): add base program structure

--- tovald/main.py
+++ tovald/main.py
@@ -1,5 +1,45 @@
-def main():
-    print("Hello Tovald")
+"""Main Tovald module."""
+
+from pathlib import Path
+
+
+def validate_documentation_tree(path: Path) -> None:
+    """Make sure documentation tree is properly structured.
+
+    Args:
+    ----
+        path (str): documentation tree root path
+    """
+
+    raise NotImplementedError
+
+
+def build_sphinx_tree(path: Path) -> None:
+    """Enhance naked documentation tree to with sphix structure.
+
+    Args:
+    ----
+        path (str): documentation tree root path
+    """
+
+    raise NotImplementedError
+
+
+def toctree_indexer(path: Path) -> None:
+    """Inject toctree node in every tree index that has children pages.
+
+    Args:
+    ----
+        path (str): documentation tree root path
+    """
+
+    raise NotImplementedError
+
+
+def main() -> None:
+    """Program entrypoint."""
+
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
