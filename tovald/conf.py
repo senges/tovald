222a80e54b652998158e954a7afad9764e75580e
Charles SENGES <charles.senges.io+github@protonmail.com>
2024-11-28 16:27:25 +0100

    feat(conf.py): enable publish force, page hierarchy, disable notification, generated titles

--- tovald/conf.py
+++ tovald/conf.py
@@ -8,6 +8,11 @@ extensions = [
 ]
 
 confluence_publish = True
+confluence_publish_force = True
+confluence_page_hierarchy = True
+confluence_disable_notifications = True
+confluence_disable_autogen_title = True
+
 confluence_server_url = os.environ.get("CONFLUENCE_SERVER_URL")
 confluence_space_key = os.environ.get("CONFLUENCE_SPACE_KEY")
 confluence_parent_page = os.environ.get("CONFLUENCE_PARENT_PAGE")
