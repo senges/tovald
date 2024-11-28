c62eda03e7dd78ade60a6e3005ad2bf146f4bd2e
Charles SENGES <charles.senges.io+github@protonmail.com>
2024-11-28 16:17:42 +0100

    feat(conf.py): fetch most basic variables from env variables

--- tovald/conf.py
+++ tovald/conf.py
@@ -1,8 +1,14 @@
 """Tovald configuration module."""
 
+import os
+
 extensions = [
     "myst_parser",
     "sphinxcontrib.confluencebuilder",
 ]
 
 confluence_publish = True
+confluence_server_url = os.environ.get("CONFLUENCE_SERVER_URL")
+confluence_space_key = os.environ.get("CONFLUENCE_SPACE_KEY")
+confluence_parent_page = os.environ.get("CONFLUENCE_PARENT_PAGE")
+confluence_publish_token = os.environ.get("CONFLUENCE_PAT")
