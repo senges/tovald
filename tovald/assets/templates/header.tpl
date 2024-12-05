caa7e3292a33038c85cfd49de5646bcc2aab10bb
Charles SENGES <charles.senges.io+github@protonmail.com>
2024-12-05 18:40:34 +0100

    feat: add page header to explicit page was generated automatically.

---
+++ tovald/assets/templates/header.tpl
@@ -0,0 +1,4 @@
+<div style="color: #707070; font-size: 12px;">
+    This page was generated automatically.
+</div>
+<hr/>
--- tovald/conf.py
+++ tovald/conf.py
@@ -17,3 +17,5 @@ confluence_server_url = os.environ.get("CONFLUENCE_SERVER_URL")
 confluence_space_key = os.environ.get("CONFLUENCE_SPACE_KEY")
 confluence_parent_page = os.environ.get("CONFLUENCE_PARENT_PAGE")
 confluence_publish_token = os.environ.get("CONFLUENCE_PAT")
+
+confluence_header_file = "assets/templates/header.tpl"
