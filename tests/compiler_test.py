5d56334dc1049554bc75fdbedba55c2fd658ed02
Charles SENGES <charles.senges.io+github@protonmail.com>
2025-03-28 10:59:24 +0100

    tests(compiler_test.py): add asset compiler test suite skeleton

--- tests/compiler_test.py
+++ tests/compiler_test.py
@@ -1,1 +1,27 @@
 """Test suite for compiler module."""
+
+import pytest
+
+
+class TestAssetsCompiler:
+    """Test suite for AssetsCompiler class."""
+
+    def test_init(self) -> None:
+        """Tests class instance initialization."""
+        pytest.fail("Not yet implemented")
+
+    def test_register(self) -> None:
+        """Tests file compiler registration decorator."""
+        pytest.fail("Not yet implemented")
+
+    def test_register_invalid_compiler(self) -> None:
+        """Tests file compiler registration decorator with invalid compiler."""
+        pytest.fail("Not yet implemented")
+
+    def test_compile_single_compiler(self) -> None:
+        """Tests asset compilation with single compiler."""
+        pytest.fail("Not yet implemented")
+
+    def test_compile_multiple_compilers(self) -> None:
+        """Tests asset compilation with multiple compilers."""
+        pytest.fail("Not yet implemented")
